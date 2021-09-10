f99edf3d8689cbb501c8a6f70129c6cc71e1b66b
Alzenth <alexbcent@hotmail.com>
2021-09-10 03:42:19 -0500

    Movimiento y cámara del jugador

---
+++ Assets/Scripts/CámaraPrimeraPersona.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CámaraPrimeraPersona : MonoBehaviour
+{
+    public float sensibilidad = 100f;
+    public Transform jugadorGráficos;
+    float rotaciónX = 0f;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensibilidad * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensibilidad * Time.deltaTime;
+        rotaciónX -= mouseY;
+        rotaciónX = Mathf.Clamp(rotaciónX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotaciónX, 0f, 0f);
+        jugadorGráficos.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public CharacterController controlador;
+
+    public float velocidadMovimiento = 10f;
+    public float gravedad = -10f;
+    public float saltoAltura = 3f;
+
+    public Transform controlSuelo;
+    public float distanciaSuelo = 0.4f;
+    public LayerMask sueloFiltro;
+
+    Vector3 velocidad;
+    bool enSuelo;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        enSuelo = Physics.CheckSphere(controlSuelo.position, distanciaSuelo, sueloFiltro);
+        
+        if(enSuelo && velocidad.y < 0)
+        {
+            velocidad.y = -2f;
+        }
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movimiento = transform.right * x + transform.forward * z;
+
+        controlador.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+
+        if(Input.GetButton("Jump") && enSuelo)
+        {
+            velocidad.y = Mathf.Sqrt(saltoAltura * -2f * gravedad);
+        }
+        velocidad.y += gravedad * Time.deltaTime;
+
+        controlador.Move(velocidad * Time.deltaTime);
+    }
+}
