d32aa7d935b33b4e45f0e03d4f2e6eda7d0904be
SharpWalker14 <78460880+SharpWalker14@users.noreply.github.com>
2021-09-15 21:04:24 -0500

    Escenas no jugables y cambio de escena

---
+++ Assets/Scripts/EscenaCambios.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscenaCambios : MonoBehaviour
+{
+  
+    //Esto es para ir a cualquier escena, solo se debe cambiar el texto por 
+    //el nombre de la escena sonde se desea ir
+    public void CambioEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+    //Esto es para salir aunque no pasa nada por que estamos en unity,pero en el compilado si funciona
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
+
