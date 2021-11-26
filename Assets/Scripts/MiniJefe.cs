f158aed3627d82f8f071b09e27d005d7e9d511ba
SharpWalker14 <78460880+SharpWalker14@users.noreply.github.com>
2021-11-26 17:28:20 -0500

    Revisión de niveles

--- Assets/Scripts/MiniJefe.cs
+++ Assets/Scripts/MiniJefe.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class MiniJefe : MonoBehaviour
 {
-
+    public float locacion;
+    public float ancho, largo, velocidad;
+    private Vector3 posicionActual;
     // Start is called before the first frame update
     void Start()
     {
-        
+        posicionActual = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movimiento();
     }
+
+    void Movimiento()
+    {
+        locacion += Time.deltaTime * velocidad;
+        float x = Mathf.Cos(locacion) * ancho;
+        float z = Mathf.Sin(locacion) * largo;
+
+        transform.position = new Vector3(posicionActual.x+x, posicionActual.y+0, posicionActual.z+z);
+
+    }
+
 }
