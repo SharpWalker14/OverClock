fcac8a10910775c0880f226ee1a24ef326396893
SharpWalker14 <78460880+SharpWalker14@users.noreply.github.com>
2021-12-03 16:09:07 -0500

    Corrección de pantallas y reorganización de mecánica de minijefe

--- Assets/Scripts/MiniJefe.cs
+++ Assets/Scripts/MiniJefe.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class MiniJefe : MonoBehaviour
 {
     public bool detectado, limite;
-    public Detector atacante, limitadorCorrer, limitadorAtaque;
+    public Detector atacante, limitador;
+    public GameObject[] patrullajes;
     private ValorSalud armadura;
     public Rigidbody cuerpo;
-    public GameObject marca, puntero, empujador, limitrofe;
+    public GameObject marca, puntero, empujador;
     public GameObject es1, es2, es3, es4;
     private GameObject jugador;
     public float tiempoCorrer, tiempoReposo, tiempoAtaque, hipotenusa, autoHipotenusa,
         locacion, disJugador, disActual,
         angulo, anguloRadian, proAngulo, proAnguloRadian, anguloContra, anguloJugador;
     public float ancho, largo, velocidad, velEmbestida;
-    private int ataque, empuje;
+    public int ataque, empuje, moverPat, numeroPat, descansoFijar;
     private Vector3 posicionActual, posJugador, posActual;
-
+    private Vector3[] posPat;
     public enum Estado { esperar, correr, atacar, cansancio };
     public Estado patrones;
     private bool candado = false;
@@ -27,13 +28,23 @@ public class MiniJefe : MonoBehaviour
         armadura = GetComponent<ValorSalud>();
         tiempoCorrer = 0;
         tiempoReposo = 0;
+        numeroPat = 0;
         ataque = 0;
+        moverPat = 0;
+        descansoFijar = 0;
         cuerpo = GetComponent<Rigidbody>();
         jugador = GameObject.FindGameObjectWithTag("Player");
         candado = true;
         locacion = -1.5f;
         posicionActual = transform.localPosition;
         posicionActual.z += largo;
+        posPat = new Vector3[patrullajes.Length];
+        for(int i = 0; i < posPat.Length; i++)
+        {
+            posPat[i].x = patrullajes[i].transform.position.x;
+            posPat[i].z = patrullajes[i].transform.position.z;
+            posPat[i].y = 0;
+        }
     }
 
     // Update is called once per frame
@@ -127,7 +138,7 @@ public class MiniJefe : MonoBehaviour
         {
             detectado = false;
         }
-        if ((limitadorCorrer.tocado == false && patrones == Estado.correr) || (limitadorAtaque.tocado == false && patrones == Estado.atacar))
+        if ((limitador.tocado == false && patrones == Estado.correr) || (limitador.tocado == false && patrones == Estado.atacar))
         {
             if (tiempoAtaque >= 0.1f)
             {
@@ -187,6 +198,7 @@ public class MiniJefe : MonoBehaviour
     {
         tiempoReposo = 0;
         tiempoCorrer += Time.deltaTime;
+        /*
         locacion -= Time.deltaTime * velocidad;
         float x = Mathf.Cos(locacion) * ancho;
         float z = Mathf.Sin(locacion) * largo;
@@ -204,6 +216,33 @@ public class MiniJefe : MonoBehaviour
         {
             patrones = Estado.atacar;
             puntero.transform.position = posJugador;
+        }
+        */
+
+        if (moverPat == 0)
+        {
+            puntero.transform.position = posPat[numeroPat];
+            moverPat++;
+        }
+        Vector3 fijador = (puntero.transform.position - posActual).normalized;
+        float rotacion = Mathf.Atan2(fijador.x, fijador.z);
+        rotacion = rotacion * (180 / Mathf.PI);
+        transform.eulerAngles = new Vector3(0, rotacion, 0);
+        if (moverPat == 1)
+        {
+            puntero.transform.position = new Vector3(0, 0, 0);
+            puntero.transform.localPosition = new Vector3(0, 0, 10);
+        }
+        fijador.y = cuerpo.velocity.y;
+        cuerpo.velocity = fijador * velocidad;
+        if (limitador.tocado && limitador.objetoRegistrado == patrullajes[numeroPat])
+        {
+            numeroPat++;
+            moverPat = 0;
+            if (numeroPat >= patrullajes.Length)
+            {
+                numeroPat = 0;
+            }
         }
     }
 
@@ -252,8 +291,6 @@ public class MiniJefe : MonoBehaviour
         if (candado == false)
         {
             marca.transform.localPosition = new Vector3(0, 0, largo);
-            limitrofe.transform.position = marca.transform.position;
-            limitrofe.transform.localScale = new Vector3(ancho * 2, 1, largo * 2);
             es1.transform.localPosition = new Vector3(0, 2, largo);
             es2.transform.localPosition = new Vector3(0, 2, -largo);
             es3.transform.localPosition = new Vector3(ancho, 2, 0);
